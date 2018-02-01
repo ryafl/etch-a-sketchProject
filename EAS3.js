ebb4b54e9fb7ad1e2dff69ea4f3ba11ae2c57eae
Ryan Flack <ryaflack@gmail.com>
2018-02-01 15:44:28 -0600

    Finished Project. Random Color Added on MouseEnter

--- EAS3.js
+++ EAS3.js
@@ -28,7 +28,7 @@ function startGame() {
 	let gridCont = document.createElement('div');
 	gridCont.classList.add('gridContainer');
 
-	gridCont.setAttribute('style', 'display: grid; background-color: blue; max-width: 960px; max-height: 960px; grid-template-columns:' + columns);
+	gridCont.setAttribute('style', 'display: grid; grid-gap: 1px; padding: 5px; background-color: #376d17; width: 960px; height: 600px; grid-template-columns:' + columns);
 	container1.appendChild(gridCont);
 
 	let startFrom1Counter = numbRAC + 1;
@@ -38,9 +38,15 @@ function startGame() {
 
 	for (let j = 1; j < numOfSquares; j++) {
 		let nameOfClass = 'GridItem' + j.toString();
+		//let nameOfClass = 'GridItem';
 		let gridItem = document.createElement('div');
 		gridItem.classList.add(nameOfClass);
-		gridItem.textContent = j.toString();
+		//gridItem.textContent = j.toString();
+		gridItem.setAttribute('style', 'background-color: #a0d481');
+		gridItem.addEventListener('mouseenter', function() {
+			
+			changeColor(j);
+		});
 		gridCont.appendChild(gridItem);
 
 
@@ -48,3 +54,24 @@ function startGame() {
 
 
 }
+
+function changeColor(classLoc) {
+	
+	let identifier = classLoc;
+	let placer = classLoc + 1;
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+
+	let colorOf = 'rgb(' + r +', ' + g + ', ' + b + ');';
+
+	let nameIt = "div.GridItem" + identifier.toString();
+	let correctOrder = "div.GridItem" + placer.toString();
+	let container = document.querySelector('div.gridContainer');
+	let colorIt = document.querySelector(nameIt);
+	let placeHolder = document.querySelector(correctOrder);
+
+	colorIt.setAttribute('style', 'background-color: '+ colorOf);
+	container.insertBefore(colorIt, placeHolder);
+
+} 
